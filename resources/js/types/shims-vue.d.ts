5aa6a87193985dffdff886f2ac003172acde81c3
David Yell <neon1024@gmail.com>
2025-02-24 17:21:25 +0000

    [shim] Not sure the shim is the best approach

--- resources/js/types/shims-vue.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Create a global shim to tell TypeScript that .vue files are valid modules.
- */
-declare module "*.vue" {
-    import { DefineComponent } from "vue";
-    const component: DefineComponent<{}, {}, any>;
-    export default component;
-}
